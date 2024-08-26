529345904ec578daa1aae02324aaa0bbdbb9b5cb
unknown <tigerpython101@gmail.com>
2024-08-27 00:02:11 +0700

    Roughly created first food item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import FoodItem from "./components/FoodItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to be awesome.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Desserts</h1>
+      <div className="foodItems">
+        <FoodItem />
+      </div>
+    </>
   );
 }
 
